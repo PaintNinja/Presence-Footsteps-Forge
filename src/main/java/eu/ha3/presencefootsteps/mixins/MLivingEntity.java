068080110f394ae1f75d7dc43c16af2ba1a0c1c3
Paint_Ninja <PaintNinja@users.noreply.github.com>
2026-02-08 15:36:50 +0000

    Remap Yarn->MojMap, remove update checker and ModMenu integration
    We'll use Forge's built-in update checker later. ModMenu integration isn't needed as a mod menu is built-in on Forge, too.

--- src/main/java/eu/ha3/presencefootsteps/mixins/MLivingEntity.java
+++ src/main/java/eu/ha3/presencefootsteps/mixins/MLivingEntity.java
@@ -1,19 +1,18 @@
 package eu.ha3.presencefootsteps.mixins;
 
 import java.util.Optional;
-
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 
 import eu.ha3.presencefootsteps.sound.SoundEngine;
 import eu.ha3.presencefootsteps.sound.StepSoundSource;
 import eu.ha3.presencefootsteps.sound.generator.StepSoundGenerator;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 
 @Mixin(LivingEntity.class)
 abstract class MLivingEntity extends Entity implements StepSoundSource {
     MLivingEntity() {super(null, null);}
-    private final StepSoundSource stepSoundSource = new StepSoundSource.Container((LivingEntity)(Object)this);
+    private final StepSoundSource stepSoundSource = new Container((LivingEntity)(Object)this);
     @Override
     public Optional<StepSoundGenerator> getStepGenerator(SoundEngine engine) {
         return stepSoundSource.getStepGenerator(engine);
